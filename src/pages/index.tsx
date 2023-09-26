f6ca46b1b8b0acce19012f933c6ebfa1eb3c26f0
paulbg <paulbg@hey.com>
2023-09-26 18:21:40 +0300

    Implement index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,33 @@
+import Link from "next/link";
+
 const Home = () => {
-  return <div>Hello world</div>;
+  return (
+    <div className="hero min-h-screen">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-5xl font-bold">What Role?! 🤖</h1>
+          <div className="py-6">
+            <p>
+              Do currently have a project where you ask that question to
+              yourself?
+            </p>
+            <p className="mt-1">Our AI-powered quiz can help you with that!</p>
+          </div>
+          <div className="flex gap-3 justify-center">
+            <Link href="/quiz" className="btn btn-primary">
+              Start
+            </Link>
+            <Link
+              href="https://github.com/paulbgtr/whatrole"
+              className="btn btn-primary"
+            >
+              Github
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
